ed6145e8fcb7b819404f6cfa65aeb6d3aa0e732d
Igoroko <plaksinsasa@mail.ru>
2020-04-04 04:15:07 +0500

    +NewFile

--- app/src/test/java/com/example/napoleon/ExampleUnitTest.kt
+++ app/src/test/java/com/example/napoleon/ExampleUnitTest.kt
@@ -4,16 +4,82 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
+
+
+
+
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+        *df02.04
+        */
+
+import org.junit.Test
+
 class ExampleUnitTest {
-    fun formatPrice(arg1: Int = 123, arg2: String = "kg", arg3: Int =5): String = "Tovare price - $arg1/$arg2, skidka - $arg3"
+
+    @Test
+    fun example() {
+
+        val iphoneCase = Product(price = 123.5, salePercent = 30)
+        val samsungCase = Product(price = 124.5, salePercent = 15)
+
+        val pricePrinter: PricePrinter = ConsolePricePrinter()
 
 
-    fun addition_fun(){
-        print(message = formatPrice())
+
+        val discountIphoneCasePrice = iphoneCase.calcDiscountPrice()
+        pricePrinter.print(discountIphoneCasePrice)
+
+        val discountSamsungCasePrice = samsungCase.calcDiscountPrice()
+        pricePrinter.print(discountSamsungCasePrice)
+
+        val products = list0f(iphoneCase, samsungCase)
+        products.forEach { product ->
+            val discountPrice = product.calcDiscountPrice()
+            pricePrinter.print(discountPrice)
+        }
+
+        val a = 1
+        val b = 2
+        val c = 3
+        val list = list0f(a,b,c)
+        list.forEach({ int ->
+            println(int)
+        })
+
+
+
+    }
+}
+
+class Product(
+    /**
+     * Must be positive
+     */
+    private val price: Double,
+    private val salePercent: Int = 0
+) {
+    /**
+     * @return price with applied discount determined by [salePercent]
+     *
+     * If [salePercent] is more than 100 than product will have negative price
+     * If [salePercent] less than 0 product price will be increased
+     */
+    fun calcDiscountPrice(): Double = price * (1 - salePercent / 100.0)
+}
+
+interface PricePrinter {
+
+    /**
+     * Outputs price in <PRICE>P format.
+     * If price have not fractional part than it will be printed as integer
+     * If price have fractional part than it will be rounded for 2 symbols after "."
+     */
+    fun print(price: Double)
+}
+
+class ConsolePricePrinter : PricePrinter {
+
+    override fun print(price: Double) {
+        println(price)
     }
 }
